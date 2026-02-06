d596439a111281de6d393a4e6e9322e06eb14078
camerasProject <backtoprimeat60@gmail.com>
2026-02-06 22:11:43 +0530

    First Class

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import First, { First1, First2 } from './First-Class/First'
+
+function App() {
+
+  return (
+    <>
+      {/* Calling the component */}
+      <First/> 
+      <First1/>
+      <First2/>
+    </>
+  )
+}
+
+export default App
---
+++ src/First-Class/First.jsx
@@ -0,0 +1,19 @@
+function First() {
+  // This is for JavaScript
+  return (
+    // This for HTML
+    <>
+      <div>This First Component</div>
+      <h1>Hello</h1>
+    </>
+  );
+}
+export default First;
+
+export function First1() {
+  return <div>This is the Second Component</div>;
+}
+
+export function First2() {
+  return <div>This is the Third Component</div>;
+}
